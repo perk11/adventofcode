9cabd5cb884d46a386f6f79f460ebe0af5f2212a
Konstantin Pereiaslov <perk11@perk11.info>
2024-12-07 14:33:44 -0600

    2024 day 3

---
+++ 2024/day3/day3_part1.go
@@ -0,0 +1,98 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func main() {
+	filenames := []string{"testInput", "input"}
+	for _, fileName := range filenames {
+		fileContents, err := os.ReadFile(fileName)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println(fileName)
+		var total int
+		for index := 0; index+len(PREFIX)+4 < len(fileContents); index++ {
+			int1, int2 := tryReadStartingFromByte(fileContents, index, PREFIX)
+			if int1 == nil {
+				continue
+			}
+			total += *int1 * *int2
+
+		}
+		println(total)
+	}
+}
+
+const PREFIX = "mul("
+const COMMA = ","
+const SUFFIX = ")"
+
+func tryReadStartingFromByte(fileContents []byte, readStartIndex int, PREFIX string) (*int, *int) {
+
+	prefix := string(fileContents[readStartIndex : readStartIndex+len(PREFIX)])
+	if prefix != PREFIX {
+		return nil, nil
+	}
+
+	var firstNumberString string
+	var firstNumberStartingIndex int
+	for firstNumberStartingIndex = readStartIndex + len(PREFIX); ; firstNumberStartingIndex++ {
+		if firstNumberStartingIndex+3 >= len(fileContents) {
+			//we need at least 3 characters ",1)" to finish reading, so if it's EOF, there is no valid string beyond
+			return nil, nil
+		}
+		currentByteString := string(fileContents[firstNumberStartingIndex])
+		if currentByteString == COMMA {
+			break
+		}
+		_, err := strconv.Atoi(currentByteString)
+		if err == nil {
+			firstNumberString += currentByteString
+		} else {
+			//found something that doesn't convert to int before encountering a comma
+			return nil, nil
+		}
+	}
+	if len(firstNumberString) == 0 {
+		return nil, nil
+	}
+
+	//firstNumberStartingIndex is at COMMA position now
+	var secondNumberString string
+	for secondNumberStartingIndex := firstNumberStartingIndex + len(COMMA); ; secondNumberStartingIndex++ {
+		if secondNumberStartingIndex+1 >= len(fileContents) {
+			//we need at least ")" to finish reading, so if it's EOF, there is no valid string beyond
+			return nil, nil
+		}
+		currentByteString := string(fileContents[secondNumberStartingIndex])
+		if currentByteString == SUFFIX {
+			break
+		}
+		_, err := strconv.Atoi(currentByteString)
+		if err == nil {
+			secondNumberString += currentByteString
+		} else {
+			//found something that doesn't convert to int before encountering a comma
+			return nil, nil
+		}
+	}
+
+	if len(secondNumberString) == 0 {
+		return nil, nil
+	}
+
+	firstNumber, err := strconv.Atoi(firstNumberString)
+	if err != nil {
+		panic(err)
+	}
+	secondNumber, err := strconv.Atoi(secondNumberString)
+	if err != nil {
+		panic(err)
+	}
+
+	return &firstNumber, &secondNumber
+}
